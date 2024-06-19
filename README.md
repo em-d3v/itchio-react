163169b8898cb40ef27abbfc8fe483e0a5d82e99
evm96m@umsystem.edu <em_dev_9836@outlook.com>
2024-06-19 14:45:54 -0500

    updated

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Getting Started with Create React App
+# Itch.io React
+
+made with:
+- Tiled
+- Reactjs
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/lib/defaults.js
@@ -0,0 +1,12 @@
+/**
+ * game constants
+ * @type {Object}
+ */
+const defaults = {
+  player:{
+    scale: [1,0,1],
+    speed: 0,
+    camera: {fov:50,aspect: 2,near: 1, far: 100}
+  }
+}
+export default defaults;
